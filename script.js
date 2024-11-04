d6fe4466e8b3f8d7a3bd12412bf37bffbd282c31
Danix43 <daninitu39@gmail.com>
2024-11-04 21:41:44 +0200

    key sign test

--- script.js
+++ script.js
@@ -4,129 +4,128 @@ const resetButton = document.querySelector("#reset");
 console.log(resetButton);
 
 const processData = async function () {
-  const formData = new FormData(form);
-  const [initialUtil, finalUtil, initialQ, finalQ, marUtil] = formData.values();
+	const formData = new FormData(form);
+	const [initialUtil, finalUtil, initialQ, finalQ, marUtil] = formData.values();
 
-  if (initialUtil && finalUtil && initialQ && finalQ && marUtil) alert("all the fields are populated, remove one");
+	if (initialUtil && finalUtil && initialQ && finalQ && marUtil) alert("all the fields are populated, remove one");
 
-  if (!initialUtil && (marUtil, finalUtil, initialQ, finalQ)) alert("empty initial Util")
+	if (!initialUtil && (marUtil, finalUtil, initialQ, finalQ)) alert("empty initial Util")
 
-  if (!finalUtil && (initialUtil, finalUtil, initialQ, finalQ)) alert("empty final Util")
+	if (!finalUtil && (initialUtil, finalUtil, initialQ, finalQ)) alert("empty final Util")
 
-  if (!initialQ && (marUtil, finalUtil, initialUtil, finalQ)) alert(`initialQ: ${await calcInitialQ(
-    initialQ, finalUtil, finalQ, marUtil
-  )}`)
+	if (!initialQ && (marUtil, finalUtil, initialUtil, finalQ)) alert(`initialQ: ${await calcInitialQ(
+		initialQ, finalUtil, finalQ, marUtil
+	)}`)
 
-  if (!finalQ && (marUtil, finalUtil, initialQ, initialUtil)) alert(`finalQ: ${await calcFinalQ(
-    initialUtil, finalUtil, initialQ, marUtil
-  )}`)
+	if (!finalQ && (marUtil, finalUtil, initialQ, initialUtil)) alert(`finalQ: ${await calcFinalQ(
+		initialUtil, finalUtil, initialQ, marUtil
+	)}`)
 
-  if (!marUtil && (initialUtil, finalUtil, initialQ, finalQ))
-    alert(
-      `marginal util: ${await calcMarginalUtil(
-        initialUtil,
-        finalUtil,
-        initialQ,
-        finalQ
-      )}`
-    );
+	if (!marUtil && (initialUtil, finalUtil, initialQ, finalQ))
+		alert(
+			`marginal util: ${await calcMarginalUtil(
+				initialUtil,
+				finalUtil,
+				initialQ,
+				finalQ
+			)}`
+		);
 };
 
 const calcMarginalUtil = async function (
-  initialUtil,
-  finalUtil,
-  initialQ,
-  finalQ
+	initialUtil,
+	finalUtil,
+	initialQ,
+	finalQ
 ) {
-  // formula: MU = (QF - QI) / (UF - UI)
-  const deltaQ = finalQ - initialQ;
-  const deltaUtil = finalUtil - initialUtil;
+	// formula: MU = (UF - UI) / (QF - QI)
+	const deltaQ = finalQ - initialQ;
+	const deltaUtil = finalUtil - initialUtil;
 
-  return deltaUtil / deltaQ;
+	return deltaUtil / deltaQ;
 };
 
 const calcInitialUtil = async function (
-  finalUtil,
-  initialQ,
-  finalQ,
-  marUtil
+	finalUtil,
+	initialQ,
+	finalQ,
+	marUtil
 ) {
+	let deltaQ = -finalQ - initialQ // 4
 
-  // 5 = 
-  // formula: MU = ()
 }
 
 const calcFinalUtil = async function (
-  initialUtil,
-  initialQ,
-  finalQ,
-  marUtil
+	initialUtil,
+	initialQ,
+	finalQ,
+	marUtil
 ) {
 
 }
 
 const calcInitialQ = async function (
-  initialUtil,
-  finalUtil,
-  finalQ,
-  marUtil
+	initialUtil,
+	finalUtil,
+	finalQ,
+	marUtil
 ) {
-  const deltaUtil = finalUtil - initialUtil;
+	const deltaUtil = finalUtil - initialUtil;
 
-  // 5 = marUtil * (x - initialQ)
-  // deltaUtil = marUtil * (x - initialQ)
+	// 5 = marUtil * (x - initialQ)
+	// deltaUtil = marUtil * (x - initialQ)
 
-  // marUtil - (marUtil * initialQ)
-  let x1 = marUtil * finalQ;
+	// marUtil - (marUtil * initialQ)
+	let x1 = marUtil * finalQ;
 
-  // marUtil = deltaUtil + x1
-  x1 = deltaUtil + x1; // 7,5
+	// marUtil = deltaUtil + x1
+	x1 = deltaUtil + x1; // 7,5
 
-  x1 = x1 * 100;
-  let x2 = marUtil * 100;
+	x1 = x1 * 100;
+	let x2 = marUtil * 100;
 
-  return x1 / x2;
+	return x1 / x2;
 }
 
 const calcFinalQ = async function (
-  initialUtil,
-  finalUtil,
-  initialQ,
-  marUtil
+	initialUtil,
+	finalUtil,
+	initialQ,
+	marUtil
 ) {
 
-  const deltaUtil = finalUtil - initialUtil;
+	const deltaUtil = finalUtil - initialUtil;
 
-  // 5 = marUtil * (x - initialQ)
-  // deltaUtil = marUtil * (x - initialQ)
+	// 5 = marUtil * (x - initialQ)
+	// deltaUtil = marUtil * (x - initialQ)
 
-  // marUtil - (marUtil * initialQ)
-  let x1 = marUtil * initialQ;
+	// marUtil - (marUtil * initialQ)
+	let x1 = marUtil * initialQ;
 
-  // marUtil = deltaUtil + x1
-  x1 = deltaUtil + x1; // 7,5
+	// marUtil = deltaUtil + x1
+	x1 = deltaUtil + x1; // 7,5
 
-  x1 = x1 * 100;
-  let x2 = marUtil * 100;
+	x1 = x1 * 100;
+	let x2 = marUtil * 100;
 
-  return x1 / x2;
+	return x1 / x2;
 }
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  processData();
+	e.preventDefault();
+	processData();
 });
 
 resetButton.addEventListener("click", e => {
-  const initialUtilInput = document.querySelector("#initialUt");
-  const finalUtilInput = document.querySelector("#finalUt");
-  const initialQInput = document.querySelector("#initialQConsumed");
-  const finalQInput = document.querySelector("#finalQConsumed");
-  const marUtilInput = document.querySelector("#marUt");
+	const initialUtilInput = document.querySelector("#initialUt");
+	const finalUtilInput = document.querySelector("#finalUt");
+	const initialQInput = document.querySelector("#initialQConsumed");
+	const finalQInput = document.querySelector("#finalQConsumed");
+	const marUtilInput = document.querySelector("#marUt");
 
-  initialUtilInput.value = "";
-  finalUtilInput.value = "";
-  initialQInput.value = "";
-  finalQInput.value = "";
-  marUtilInput.value = "";
+	initialUtilInput.value = "";
+	finalUtilInput.value = "";
+	initialQInput.value = "";
+	finalQInput.value = "";
+	marUtilInput.value = "";
 })
